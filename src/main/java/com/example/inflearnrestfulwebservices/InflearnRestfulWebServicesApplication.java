fcb22b3ff1cbca50b6e5bdaadbc096ca6da2aeb4
jinkyoung-jeon <73870038+jinkyoung-jeon@users.noreply.github.com>
2022-10-05 00:09:36 +0900

    spring boot를 이용한 restful web services 개발 / helloworld controller 추가

---
+++ src/main/java/com/example/inflearnrestfulwebservices/InflearnRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnRestfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inflearnrestfulwebservices/InflearnRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
