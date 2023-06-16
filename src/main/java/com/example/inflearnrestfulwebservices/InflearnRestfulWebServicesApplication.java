1f894ed28ea3a69b740aceaaf4523a8ea0c1713d
JeonJinKyoung <kyeong1750@naver.com>
2023-06-17 04:31:52 +0900

    다국어 처리를 위한 Internationalization 구현 방법

--- src/main/java/com/example/inflearnrestfulwebservices/InflearnRestfulWebServicesApplication.java
+++ src/main/java/com/example/inflearnrestfulwebservices/InflearnRestfulWebServicesApplication.java
@@ -2,12 +2,22 @@ package com.example.inflearnrestfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class InflearnRestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InflearnRestfulWebServicesApplication.class, args);
+	}
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
 	}
 
 }
